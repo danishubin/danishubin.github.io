e9b8ce1b24690fec2f9ed6a8d17e1d7c354cfc37
danishubin <dshubin@eastsideprep.org>
2023-02-12 12:44:52 -0800

    CS Gallery

---
+++ portfolio/js/script.js
@@ -0,0 +1,16 @@
+navLogo = document.getElementsByClassName("nav-logo")[0];
+
+// function navLogoScroll() {
+//     var windowHeight = window.innerHeight;
+//     var elementVisible = 150;
+//     if (windowHeight - elementVisible > 0) {
+//         navLogo.style.display = "inherit";
+//     } else {
+//         navLogo.style.display = "inherit";
+//     }
+// }
+
+// window.addEventListener("scroll", navLogoScroll);
+
+// // To check the scroll position on page load
+// navLogoScroll();
