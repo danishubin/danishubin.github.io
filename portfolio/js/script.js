2b84348be06474dcb410084d999ca5dceac7689b
danishubin <dshubin@eastsideprep.org>
2023-02-14 13:09:32 -0800

    updated contact and page animation

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -2,6 +2,11 @@ navLogo = document.getElementsByClassName("nav-logo")[0];
 
 
 function pageAnimate() {
+    // Each time page is reloaded, scroll up
+    history.scrollRestoration = "manual";
+    window.onbeforeunload = function() {
+        window.scrollTo(0, 0);
+    }
     var layers = document.getElementsByClassName("top-layer");
     var fadeIn = document.getElementsByClassName("fade-in")[0];
     for (var j = 0; j < layers.length; j++) {
@@ -13,9 +18,3 @@ function pageAnimate() {
 window.onload = function() {
     pageAnimate();
 };
-
-// Each time page is reloaded, scroll up
-history.scrollRestoration = "manual";
-window.onbeforeunload = function() {
-    window.scrollTo(0, 0);
-}
