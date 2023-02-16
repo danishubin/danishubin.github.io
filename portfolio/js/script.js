6eb316e08059a90258f6b6087cecca2122ba3584
danishubin <dshubin@eastsideprep.org>
2023-02-15 23:50:33 -0800

    Fixed Animation transition and navbar screen size

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,8 +1,21 @@
 navLogo = document.getElementsByClassName("nav-logo")[0];
+var layers = document.getElementsByClassName("top-layer");
 
+
+layers[4].addEventListener('transitionend', (event) => {
+    transitionedFinished = setInterval(() => {
+        if (layers[4].offsetLeft == screen.width) {
+            for (var j = 0; j < layers.length; j++) {
+                layers[j].style.transition = 'none';
+                layers[j].style.opacity = '0';
+                layers[j].style.left = '-100%';
+            }
+            clearInterval(transitionedFinished);
+        }
+    }, 500)
+});
 
 function pageAnimate() {
-    var layers = document.getElementsByClassName("top-layer");
     var fadeIn = document.getElementsByClassName("fade-in")[0];
     for (var j = 0; j < layers.length; j++) {
         layers[j].classList.toggle("active");
@@ -13,14 +26,6 @@ function pageAnimate() {
         window.scrollTo(0, 0);
     }
     fadeIn.style.opacity = 1;
-    setTimeout(() => {
-        var layers = document.getElementsByClassName("top-layer");
-        for (var j = 0; j < layers.length; j++) {
-            layers[j].style.transition = 'none';
-            layers[j].style.opacity = '0';
-            layers[j].style.left = '-100%';
-        }
-    }, 1500)
 }
 
 window.onload = function() {
